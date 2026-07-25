12dd6867830adb40be7e3e8efc2cfdf9428ded59
Ryan M. Waite <ryanwaite28@gmail.com>
2026-07-25 19:30:05 -0400

    added basepath

--- next.config.prod.mjs
+++ next.config.prod.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true, // Required because Next.js image optimization needs a server
-  }
+  },
+  basePath: '/rmwllc-consulting',
 }
 
 export default nextConfig
